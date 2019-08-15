aac9cb68b62346687e3a0eceb561193f73be6e8a
Roberto Umemura <roberto.silva@novaandradina.org>
2019-08-15 08:21:25 -0400

    funcionou

--- atv4.js
+++ atv4.js
@@ -1,5 +1,13 @@
+
+// 4. Multiplique a idade de todos usuários por dois e depois realize um filtro
+// nos usuários que possuem no máximo 50 anos
 const data = require("./data.js");
 
-const idade = data.forEach(data=> console.log(data.first_name));
+const idade = data.map(data=> (data.age*2)<=50);
+const anos = data.map(data=> `${data.first_name} tem ${data.age*2}`);
+for (var i=0; i<=idade.length;i++){
 
-console.log(idade);
+if (idade[i] === true){
+    console.log(anos[i]);
+}
+}
